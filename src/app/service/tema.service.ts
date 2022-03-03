ba2b190320071c6134125c4cd08a46c8ad09e63e
Tatyana Okimura <tatyanamie@gmail.com>
2022-03-03 14:15:50 -0300

    implementação put e delete de tema

--- src/app/service/tema.service.ts
+++ src/app/service/tema.service.ts
@@ -19,8 +19,20 @@ export class TemaService {
     return this.http.get<Tema[]>('https://blogpessoaltatyana.herokuapp.com/tema',this.token)
   }
 
+  getByIdTema(id: number): Observable<Tema>{
+    return this.http.get<Tema>(`https://blogpessoaltatyana.herokuapp.com/tema/${id}`,this.token)
+  }
+
   postTema(tema: Tema): Observable<Tema>{
     return this.http.post<Tema>('https://blogpessoaltatyana.herokuapp.com/tema', tema, this.token)
+  }
+
+  putTema(tema: Tema): Observable<Tema>{
+    return this.http.put<Tema>('https://blogpessoaltatyana.herokuapp.com/tema',tema, this.token)
+  }
+
+  deleteTema(id: number){
+    return this.http.delete(`https://blogpessoaltatyana.herokuapp.com/tema/${id}`,this.token)
   }
 
 }
